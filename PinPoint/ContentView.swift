532f15f0aad30f16bec65c049abb4fc9de41c792
Linnea Sandsjo <linnea.sandsjo@gmail.com>
2025-09-15 16:21:51 -0500

    Project created

---
+++ PinPoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinPoint
+//
+//  Created by Linnea on 2025-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
