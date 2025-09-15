400febc609e0fe8556b8080e70bc7e7d869f2204
Linnea Sandsjo <linnea.sandsjo@gmail.com>
2025-09-15 16:21:51 -0500

    Project created

---
+++ PinPoint/PinPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinPointApp.swift
+//  PinPoint
+//
+//  Created by Linnea on 2025-09-14.
+//
+
+import SwiftUI
+
+@main
+struct PinPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
